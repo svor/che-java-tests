e5e9ae98b4729e15581942956597574584a86469
Josh Pinkney <joshpinkney@gmail.com>
2020-03-29 20:08:38 -0400

    Reformatted languages tests to use vscode.executeXXX API

--- tests/languages.test.ts
+++ tests/languages.test.ts
@@ -8,11 +8,10 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
-import * as testservice from '@eclipse-che/testing-service';
 import * as vscode from '@theia/plugin';
 import * as helper from './helper';
-import { extensionID } from './helper';
 import { strict as assert } from 'assert';
+import { CompletionList, Position, DocumentSymbol, Location, Hover, SymbolInformation, Range, FormattingOptions, TextEdit } from 'vscode';
 
 describe('Che-Java sample tests on Quarkus Project', () => {
 
@@ -26,25 +25,28 @@ describe('Che-Java sample tests on Quarkus Project', () => {
 
     it('Test Java Completion on sample java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const completion: any = await testservice.languageserver.completion(extensionID,
-            {
-                uri: mySampleURI
-            } as vscode.TextDocument, {
-                column: 1,
-                lineNumber: 13
-            }, {
-                triggerKind: 0
-            }, new vscode.CancellationTokenSource().token);
-        assert.notEqual(completion.length, 0, 'The completion request returned 0 results when it should have returned at least 1');
+        const args = {
+            uri: mySampleURI,
+            position: new Position(13, 1)
+        };
+        const completion = await vscode.commands.executeCommand("vscode.executeCompletionItemProvider", args) as CompletionList[];
+        if (!completion) {
+            assert.fail("Expected Completion");
+        } else {
+            assert.notEqual(completion.length, 0, 'The completion request returned 0 results when it should have returned at least 1');
+        }
     });
 
     it('Test Java Document Symbols on basic java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const symbols = await testservice.languageserver.documentSymbols(extensionID, {
+
+        const args = {
             uri: mySampleURI
-        } as vscode.TextDocument, new vscode.CancellationTokenSource().token);
-        assert.notEqual(symbols, undefined);
-        if (symbols) {
+        };
+        const symbols = await vscode.commands.executeCommand("vscode.executeDocumentSymbolProvider", args) as DocumentSymbol[];
+        if (!symbols) {
+            assert.fail("Expected document symbols");
+        } else {
             assert.equal(symbols.length, 2);
             assert.equal(symbols[0].name, "org.my.sample");
             assert.equal(symbols[1].name, "MySample");
@@ -53,13 +55,18 @@ describe('Che-Java sample tests on Quarkus Project', () => {
 
     it('Test Java implementation on basic java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const implementation = await testservice.languageserver.implementation(extensionID, {
-            uri: mySampleURI
-        } as vscode.TextDocument, {
-                column: 39,
-                lineNumber: 14
-            }, new vscode.CancellationTokenSource().token);
-        assert.notEqual(implementation, undefined);
+
+        const args = {
+            uri: mySampleURI,
+            position: new Position(14, 39)
+        };
+        const implementation = await vscode.commands.executeCommand("vscode.executeImplementationProvider", args) as Location[];
+        if (!implementation) {
+            assert.fail(
+                "Expected implementations"
+            )
+        }
+
         if (implementation && Array.isArray(implementation)) {
             assert.deepEqual(implementation[0].uri, myHelloTextURI);
             assert.deepEqual(implementation[0].range, {
@@ -70,19 +77,23 @@ describe('Che-Java sample tests on Quarkus Project', () => {
             });
         } else {
             assert.fail(
-                "Expected an array of type definitions"
+                "Expected an array of implementations"
             )
         }
     });
 
     it('Test Java type definition on basic java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const typeDefinition = await testservice.languageserver.typeDefinition(extensionID, {
-            uri: mySampleURI
-        } as vscode.TextDocument, {
-                column: 35,
-                lineNumber: 14
-            }, new vscode.CancellationTokenSource().token);
+
+        const args = {
+            uri: mySampleURI,
+            position: new Position(14, 35)
+        };
+        const typeDefinition = await vscode.commands.executeCommand("vscode.executeTypeDefinitionProvider", args) as Location[];
+        if (!typeDefinition) {
+            assert.fail("Completion");
+        }
+
         if (typeDefinition && Array.isArray(typeDefinition)) {
             assert.deepEqual(typeDefinition[0].uri, myHelloTextURI);
             assert.deepEqual(typeDefinition[0].range, {
@@ -100,42 +111,49 @@ describe('Che-Java sample tests on Quarkus Project', () => {
 
     it('Test Java hover on basic java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const hover = await testservice.languageserver.hover(extensionID, {
-            uri: mySampleURI
-        } as vscode.TextDocument, {
-                column: 35,
-                lineNumber: 14
-            }, new vscode.CancellationTokenSource().token);
-        if (hover) {
-            assert.ok((hover.contents[0] as any).value.includes("org.my.sample.MyHelloText.MyHelloText()"));
-            assert.deepEqual(hover.range, {
+
+        const args = {
+            uri: mySampleURI,
+            position: new Position(14, 35)
+        };
+        const hover = await vscode.commands.executeCommand("vscode.executeHoverProvider", args) as Hover[];
+        if (!hover) {
+            assert.fail("Completion");
+        } else {
+            assert.ok((hover[0].contents[0] as any).value.includes("org.my.sample.MyHelloText.MyHelloText()"));
+            assert.deepEqual(hover[0].range, {
                 endColumn: 35,
                 endLineNumber: 14,
                 startColumn: 35,
                 startLineNumber: 14
             });
         }
+
     });
 
     it('Test Java Workspace Symbols on basic java file', async () => {
-        const workspaceSymbols = await testservice.languageserver.workspaceSymbols(extensionID, "MySample",
-            new vscode.CancellationTokenSource().token);
+        const args = {
+            query: "MySample"
+        };
+        const workspaceSymbols = await vscode.commands.executeCommand("vscode.executeWorkspaceSymbolProvider", args) as SymbolInformation[];
+        if (!workspaceSymbols) {
+            assert.fail("Completion");
+        }
         assert.notEqual(workspaceSymbols.length, 0, 'The workspace symbols request returned 0 results when it should have returned at least 1');
         assert.equal(workspaceSymbols[0].name, "MySample");
     });
 
     it('Test Java references on basic java file', async () => {
         await vscode.window.showTextDocument(myHelloTextURI);
-        const references = await testservice.languageserver.references(extensionID, {
-            uri: myHelloTextURI
-        } as vscode.TextDocument, {
-                column: 16,
-                lineNumber: 6
-            }, {
-                includeDeclaration: true
-            },
-            new vscode.CancellationTokenSource().token);
-        if (references) {
+
+        const args = {
+            uri: myHelloTextURI,
+            position: new Position(6, 16)
+        };
+        const references = await vscode.commands.executeCommand("vscode.executeReferenceProvider", args) as Location[];
+        if (!references) {
+            assert.fail("Completion");
+        } else {
             assert.notEqual(references.length, 0, 'The references request returned 0 results when it should have returned at least 1');
             assert.deepEqual(references[0].uri.path, mySampleURI.path);
             assert.deepEqual(references[0].range, {
@@ -147,113 +165,97 @@ describe('Che-Java sample tests on Quarkus Project', () => {
         }
     });
 
-    it('Test Java document links on basic java file', async () => {
-        await vscode.window.showTextDocument(mySampleURI);
-        const renameEdits = await testservice.languageserver.renameEdits(extensionID, {
-            uri: mySampleURI
-        } as vscode.TextDocument, {
-                column: 23,
-                lineNumber: 15
-            }, "text", new vscode.CancellationTokenSource().token);
-        if (renameEdits) {
-            assert.equal(renameEdits.edits.length, 1);
-            assert.equal((renameEdits.edits as any[])[0].edits[0].text, "text = new MyHelloText();\n        return text");
-            assert.equal((renameEdits.edits as any[])[0].resource.path, mySampleURI.path);
-        } else {
-            assert.fail();
-        }
-    });
-
     it('Test Java document formatting on basic java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const formattingEdits = await testservice.languageserver.documentFormattingEdits(extensionID, {
-            uri: mySampleURI
-        } as vscode.TextDocument, {
+
+        const args = {
+            uri: mySampleURI,
+            options: {
                 insertSpaces: true,
                 tabSize: 2
-            }, new vscode.CancellationTokenSource().token);
-        if (formattingEdits) {
+            } as FormattingOptions
+        };
+        const formattingEdits = await vscode.commands.executeCommand("vscode.executeFormatDocumentProvider", args) as TextEdit[];
+        if (!formattingEdits) {
+            assert.fail("Expected formatting edits");
+        } else {
             assert.equal(formattingEdits.length, 6);
-            assert.equal(formattingEdits[0].text, "\n\n  ");
+            assert.equal(formattingEdits[0].newText, "\n\n  ");
             assert.deepEqual(formattingEdits[0].range, {
                 endColumn: 5,
                 endLineNumber: 11,
                 startColumn: 24,
                 startLineNumber: 9
             });
-            assert.equal(formattingEdits[1].text, "\n  ");
+            assert.equal(formattingEdits[1].newText, "\n  ");
             assert.deepEqual(formattingEdits[1].range, {
                 endColumn: 5,
                 endLineNumber: 12,
                 startColumn: 9,
                 startLineNumber: 11
             });
-            assert.equal(formattingEdits[2].text, "\n  ");
+            assert.equal(formattingEdits[2].newText, "\n  ");
             assert.deepEqual(formattingEdits[2].range, {
                 endColumn: 5,
                 endLineNumber: 13,
                 startColumn: 36,
                 startLineNumber: 12
             });
-            assert.equal(formattingEdits[3].text, "\n\n    ");
+            assert.equal(formattingEdits[3].newText, "\n\n    ");
             assert.deepEqual(formattingEdits[3].range, {
                 endColumn: 9,
                 endLineNumber: 15,
                 startColumn: 28,
                 startLineNumber: 13
             });
-            assert.equal(formattingEdits[4].text, "\n    ");
+            assert.equal(formattingEdits[4].newText, "\n    ");
             assert.deepEqual(formattingEdits[4].range, {
                 endColumn: 9,
                 endLineNumber: 16,
                 startColumn: 46,
                 startLineNumber: 15
             });
-            assert.equal(formattingEdits[5].text, "\n  ");
+            assert.equal(formattingEdits[5].newText, "\n  ");
             assert.deepEqual(formattingEdits[5].range, {
                 endColumn: 5,
                 endLineNumber: 17,
                 startColumn: 31,
                 startLineNumber: 16
             });
-        } else {
-            assert.fail();
         }
     });
 
     it('Test Java range formatting on basic java file', async () => {
         await vscode.window.showTextDocument(mySampleURI);
-        const formattingEdits = await testservice.languageserver.documentRangeFormattingEdits(extensionID, {
-            uri: mySampleURI
-        } as vscode.TextDocument, {
-                startColumn: 1,
-                startLineNumber: 1,
-                endColumn: 25,
-                endLineNumber: 10
-            }, {
+
+        const args = {
+            uri: mySampleURI,
+            range: new Range(new Position(1, 1), new Position(10, 25)),
+            options: {
                 insertSpaces: true,
                 tabSize: 2
-            }, new vscode.CancellationTokenSource().token);
-        if (formattingEdits) {
+            } as FormattingOptions
+        };
+        const formattingEdits = await vscode.commands.executeCommand("vscode.executeFormatRangeProvider", args) as TextEdit[];
+        if (!formattingEdits) {
+            assert.fail("Expected formatting edits");
+        } else {
             assert.equal(formattingEdits.length, 2);
-            assert.equal(formattingEdits[0].text, "\n\n  ");
+            assert.equal(formattingEdits[0].newText, "\n\n  ");
             assert.deepEqual(formattingEdits[0].range, {
                 endColumn: 5,
                 endLineNumber: 11,
                 startColumn: 24,
                 startLineNumber: 9
             });
-            assert.equal(formattingEdits[1].text, "\n  ");
+            assert.equal(formattingEdits[1].newText, "\n  ");
             assert.deepEqual(formattingEdits[1].range, {
                 endColumn: 5,
                 endLineNumber: 12,
                 startColumn: 9,
                 startLineNumber: 11
             });
-        } else {
-            assert.fail();
         }
     });
-    // tslint:enable
 
 });
